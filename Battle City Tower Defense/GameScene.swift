ab87425bf944d053975f121124200a9ba2000a52
Zhaoning Cai <caizhaoning@gmail.com>
2020-02-08 22:07:23 -0500

    Added enemy stat son file; Added enemy struct; Added static tiles on the map (to be modified)

--- Battle City Tower Defense/GameScene.swift
+++ Battle City Tower Defense/GameScene.swift
@@ -6,67 +6,40 @@
 //  Copyright © 2020 CentennialCollege. All rights reserved.
 //
 
+import UIKit
+import AVFoundation
 import SpriteKit
 import GameplayKit
 
+let screenSize = UIScreen.main.bounds
+var screenWidth: CGFloat?
+var screenHeight: CGFloat?
+
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        screenWidth = frame.width
+        screenHeight = frame.height
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Set up static tiles on the map
+        self.setUpStaticTiles()
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -85,5 +58,337 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+    
+    func setUpStaticTiles(){
+        // ** Object initiation to be cahnged to using GameObject class
+        var river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -75, y: (screenHeight!) / 2 - 75)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 75)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -75, y: (screenHeight!) / 2 - 125)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 125)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -75, y: (screenHeight!) / 2 - 175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 225)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 275)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y: (screenHeight!) / 2 - 325)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -125, y: (screenHeight!) / 2 - 175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y: (screenHeight!) / 2 - 175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y: (screenHeight!) / 2 - 325)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y: (screenHeight!) / 2 - 325)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y: (screenHeight!) / 2 - 225)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y: (screenHeight!) / 2 - 375)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y: (screenHeight!) / 2 - 275)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y: (screenHeight!) / 2 - 325)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y: (screenHeight!) / 2 - 375)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 425)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y: (screenHeight!) / 2 - 425)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 525)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 575)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -125, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -75, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 125, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 475)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 525)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 575)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 625)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 675)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 725)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 825)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 125, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -75, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -125, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 675)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 875)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 725)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 775)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 825)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 875)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 925)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 975)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 975)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1025)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1025)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1125)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1225)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1275)
+        addChild(river)
+        // Unbreakable wall starts here *******
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 25, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 75, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 125, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1075)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1125)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1225)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1275)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: 175, y:  (screenHeight!) / 2 - 1325)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -175, y:  (screenHeight!) / 2 - 1325)
+        addChild(river)
+        // Unbreakable wall ends here ******
+        // Base brick wall starts here ******
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1125)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1175)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1225)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1275)
+        addChild(river)
+        river = SKSpriteNode(imageNamed: "river")
+        river.size = CGSize(width: 50, height: 50)
+        river.position = CGPoint(x: -25, y:  (screenHeight!) / 2 - 1325)
+        addChild(river)
+        // Base brick wall ends here ******
     }
 }
