2fdae9a80a2c07e7d789e3685eb639c2f8b90865
Jason <jasonsun0603@gmail.com>
2020-02-11 15:23:34 -0500

    sound added logo added change start scene change start button

--- Battle City Tower Defense/Scenes/StartScene.swift
+++ Battle City Tower Defense/Scenes/StartScene.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 CentennialCollege. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import AVFoundation
+import SpriteKit
+import GameplayKit
+
+class StartScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+        screenWidth = frame.width
+        screenHeight = frame.height
+        self.logoset()
+
+        
+        // ***
+    }
+    
+    func logoset () {
+        let logo = SKSpriteNode(imageNamed: "logo")
+        logo.position = CGPoint(x: 0, y: 700)
+        logo.size = CGSize(width: 450, height: 200)
+            
+        addChild(logo)
+        
+        let move = SKAction.moveTo(y: 0, duration: 5)
+        logo.run(move)
+    }
+
+}
